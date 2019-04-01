0e862d6fdffd14fb026088a16d09be10e23f813b
Sibtain Ali <allroundexperts@gmail.com>
2019-04-01 16:30:29 +0500

    Added type declaration file.

---
+++ library.d.ts
@@ -0,0 +1,33 @@
+declare class OpVault {
+  constructor(profile: JSON, items: Object);
+  unlock(masterPassword: String): Promise<true>;
+  isUnlocked(): Boolean;
+  lock(): Boolean;
+  loadItems(): Object;
+  getItem(
+    title: String
+  ): Promise<{
+    overview: {
+      title: String;
+      url: String;
+      tags: Array;
+      uuid: String;
+      URLs: Array;
+    };
+    detail: {
+      fields: [
+        {
+          type: String;
+          value: String;
+          designation: String;
+          name: String;
+        }
+      ];
+      htmlForm: {
+        htmlAction: String;
+        htmlName: String;
+        htmlMethod: String;
+      };
+    };
+  }>;
+}
